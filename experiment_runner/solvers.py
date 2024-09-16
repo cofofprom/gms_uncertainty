e5f4a777975657b907b4f0d37a17b931c00d73a8
cofofprom <nightmarelandscape@gmail.com>
2024-09-17 02:46:08 +0300

    Add new solvers

--- experiment_runner/solvers.py
+++ experiment_runner/solvers.py
@@ -1,4 +1,6 @@
 from sklearn.covariance import graphical_lasso, empirical_covariance
+from utils import pearson_corr_via_kendall, pearson_corr_via_fechner
+from robust_selection import RobustSelection
 
 def graphical_lasso_via_pearson(data, reg_param):
     covar = empirical_covariance(data, assume_centered=True)
@@ -8,3 +10,26 @@ def graphical_lasso_via_pearson(data, reg_param):
         result = None
 
     return result
+
+def graphical_lasso_via_kendall(data, reg_param):
+    covar = pearson_corr_via_kendall(data)
+    try:
+        result = graphical_lasso(covar, reg_param, return_costs=True, max_iter=500)
+    except:
+        result = None
+
+    return result
+
+def graphical_lasso_via_fechner(data, reg_param):
+    covar = pearson_corr_via_fechner(data)
+    try:
+        result = graphical_lasso(covar, reg_param, return_costs=True, max_iter=500)
+    except:
+        result = None
+
+    return result
+
+def graphical_lasso_robsel(data, params):
+    param = RobustSelection(data, params)
+
+    return graphical_lasso_via_pearson(data, param)
