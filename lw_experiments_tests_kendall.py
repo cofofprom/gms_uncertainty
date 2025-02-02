192e3886ba6abe959a884ee2071ea285640f481f
Ilya Kostylev <nightmarelandscape@gmail.com>
2025-02-03 00:18:45 +0300

    Added experiments code

---
+++ lw_experiments_tests_kendall.py
@@ -0,0 +1,162 @@
+from utils import *
+import numpy as np
+from sklearn.metrics import confusion_matrix
+import multiprocessing as mp
+from tqdm import tqdm
+import pickle
+import matplotlib.pyplot as plt
+from scipy.stats import hmean
+import time
+import sys
+
+def fixed_model_experiment(covar, true_edges, config):
+    solver = MHTSolver(config['sig_alpha'], tau_pvalues, tau)
+
+    results = []
+
+    for n_repl in range(config['S_obs']):
+        res = []
+        data = np.random.multivariate_normal(np.zeros(config['N']), covar, size=config['n'])
+        solver.fit(data)
+
+        for correction in config['algs']:
+            pred_adj = solver.apply_correction(correction)
+            pred_edges = pred_adj[np.triu_indices_from(pred_adj, k=1)]
+            
+            conf = confusion_matrix(true_edges, pred_edges, labels=[0,1])
+
+            res.append(conf)
+        
+        results.append(res)
+
+    results = np.stack(results)
+
+    return results
+
+def multiple_models_experiment(density, config, n_jobs=None):
+    models = [generateDominantDiagonal(config['N'], density) for n_model in range(config['S_sg'])]
+
+    results = []
+
+    with mp.Pool(n_jobs) as pool:
+        waiters = []
+
+        for model in models:
+            covar = model[1] # We control density in the inverse matrix
+
+            true_adj = (covar != 0.).astype(int) - np.eye(config['N'])
+            true_edges = true_adj[np.triu_indices_from(true_adj, k=1)]
+
+            waiters.append(pool.apply_async(fixed_model_experiment, args=(covar, true_edges, config)))
+
+        for waiter in waiters:
+            results.append(waiter.get())
+
+    results = np.stack(results)
+
+    return results
+
+def vary_density_experiment(config, output_file, n_jobs=None):
+    results = []
+    for density in config['densities']:
+        print(f'Working on d={np.around(density, 2)}...')
+        ts_start = time.perf_counter()
+        results.append(multiple_models_experiment(density, config))
+        ts_end = time.perf_counter()
+
+        print(f"it took {np.around(ts_end - ts_start, 2)}s")
+
+    results = np.stack(results)
+
+    with open(output_file, 'wb') as f:
+        pickle.dump(results, f)
+
+    return results
+
+def draw_metrics(exp_data, config, output_file1, output_file2):
+    densities = config['densities']
+    algs = config['algs']
+
+    tn = exp_data[:, :, :, :, 0, 0]
+    fp = exp_data[:, :, :, :, 0, 1]
+    fn = exp_data[:, :, :, :, 1, 0]
+    tp = exp_data[:, :, :, :, 1, 1]
+
+    fdr = np.nan_to_num(fp / (tp + fp), nan=0)
+    fomr = np.nan_to_num(fn / (tn + fn), nan=0)
+    tpr = np.nan_to_num(tp / (tp + fn), nan=1)
+    tnr = np.nan_to_num(tn / (tn + fp), nan=1)
+
+    ba = (tpr + tnr) / 2
+    f1 = hmean([1 - fdr, tpr])
+    mcc_first = tpr * tnr * (1 - fdr) * (1 - fomr)
+    mcc_second = (1 - tpr) * (1 - tnr) * fomr * fdr
+    mcc = np.sqrt(mcc_first) - np.sqrt(mcc_second)
+
+    fig, axes = plt.subplots(2, 2)
+    metrics = [tnr, fomr, fdr, tpr]
+    mname = ['TNR', 'FOR', 'FDR', 'TPR']
+    axes_ord = axes[0, 0], axes[0, 1], axes[1, 0], axes[1, 1]
+
+    for m, ax, mn in zip(metrics, axes_ord, mname):
+        m = np.reshape(m, (len(densities), -1, len(algs)))
+        for idx, alg in enumerate(algs):
+            ys = m[:, :, idx].mean(axis=-1)
+
+            ax.plot(densities, ys, label=alg)
+
+        ax.set_ylim(0, 1)
+        ax.legend()
+        ax.grid(True)
+        ax.set_xlim(0.1, 0.9)
+        ax.set_title(mn)
+
+    fig.set_size_inches(15, 15)
+    fig.savefig(output_file1, pad_inches=0.5, bbox_inches='tight')
+
+    fig, axes = plt.subplots(1, 3)
+    metrics = [ba, f1, mcc]
+    mname = ['BA', 'F1', 'MCC']
+    axes_ord = axes
+
+    for m, ax, mn in zip(metrics, axes_ord, mname):
+        m = np.reshape(m, (len(densities), -1, len(algs)))
+        for idx, alg in enumerate(algs):
+            ys = m[:, :, idx].mean(axis=-1)
+
+            ax.plot(densities, ys, label=alg)
+        
+        ax.set_ylim(0, 1)
+        ax.legend()
+        ax.grid(True)
+        ax.set_xlim(0.1, 0.9)
+        ax.set_title(mn)
+
+    fig.set_size_inches(15, 10)
+    fig.savefig(output_file2, pad_inches=0.5, bbox_inches='tight')
+
+def main():
+    densities = np.linspace(0.1, 0.9, 20)
+
+    config = {
+        'N': 40,
+        'n': int(sys.argv[1]),
+        'S_sg': 500,
+        'S_obs': 100,
+        'sig_alpha': 0.05,
+        'densities': densities,
+        'algs': ('SI', 'B', 'H', 'BH', 'BY'),
+    }
+
+    exp_name = f'dim{config["N"]}_n{config["n"]}_Ssg{config["S_sg"]}_Sobs{config["S_obs"]}_kendall'
+    print(f"Experiment name: {exp_name}")
+
+    exp_data = vary_density_experiment(config, f'data/{exp_name}.pickle')
+    print(f'Got {exp_data.shape} shape')
+
+    draw_metrics(exp_data, config,
+                 f'plots/tpr_{exp_name}.png',
+                 f'plots/mcc_{exp_name}.png')
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,180 @@
+import networkx as nx
+import numpy as np
+from scipy.stats import t, kendalltau, norm
+
+def generateDominantDiagonal(dim: int, density: float) -> tuple:
+    # Generate random Erdos-Renyi graph with given probability of an edge (density)
+    graph = nx.gnp_random_graph(dim, density)
+    # Transform the graph to adjacency matrix
+    adj = nx.adjacency_matrix(graph).toarray()
+    
+    # Generate random matrix A where elements are uniformly distributed
+    A = np.random.uniform(0.5, 1, size=(dim, dim))
+
+    # Generate random matrix B where elements are randomly selected from the set {-1, 1}
+    B = np.random.choice([-1, 1], size=(dim, dim))
+
+    # Find the matrix prec where elements are uniformly distributed on (-1, 0.5] AND [0.5, 1)
+    # and if there's no edge between nodes i and j the value will be 0
+    # multiplication here means element-wise multiplication
+    prec = adj * A * B
+    # Find the sum of absolute values of each row (at this point diagonal contains only zeros)
+    rowsums = np.sum(np.abs(prec), axis=1)
+    rowsums[rowsums == 0] = 0.0001 # handles the situation where row is completely empty
+
+    # Scale each row by 1.5 times of corresponding row sum
+    prec = prec / (1.5 * rowsums[:, None])
+    # Make matrix symmetric and add ones on the diagonal
+    prec = (prec + prec.T) / 2 + np.eye(dim)
+
+    precision = prec
+    # Find the covariance as matrix inverse of precision (since precision is SPD)
+    covariance = np.linalg.inv(precision)
+
+    # Calculate partial correlations (just in case)
+    pD = np.diag(1 / np.sqrt(np.diag(precision))) 
+    pcorr = -(pD @ precision @ pD)
+    np.fill_diagonal(pcorr, 1)
+
+    return covariance, precision, pcorr
+
+def pcorr_pvalues(r, n, N):
+    dof = n - N
+    stat = r * np.sqrt(dof / (1 - (r ** 2)))
+    pval = 2 * t.sf(np.abs(stat), dof)
+    return pval
+
+def corr_pvalues(r, n, N=0):
+    dof = n - 2
+    stat = r * np.sqrt(dof / (1 - (r ** 2)))
+    pval = 2 * t.sf(np.abs(stat), dof)
+    return pval
+
+def tau_pvalues(r, n, N=0):
+    return r
+
+def pcorrcoef(X):
+    cov = np.cov(X)
+    prec = np.linalg.inv(cov)
+    D = np.diag(1 / np.sqrt(np.diag(prec)))
+    corr = -(D @ prec @ D)
+    np.fill_diagonal(corr, 1)
+    return corr
+
+def tau(X):
+    corr = np.zeros((X.shape[0], X.shape[0]))
+    for idx1 in range(len(X)):
+        for idx2 in range(idx1, len(X)):
+            corr[idx1, idx2] = kendalltau(X[idx1], X[idx2]).pvalue
+
+    corr += corr.T + np.eye(X.shape[0])
+
+    return corr
+
+def fechner_corr(x, y):
+    ind = ((x - np.mean(x))*(y - np.mean(y)))
+    res = (np.sum((ind >= 0)) - np.sum((ind < 0))) / len(x)
+    return res
+
+def fechnercoef(X):
+    corr = np.eye(X.shape[0])
+    for idx1 in range(len(X)):
+        for idx2 in range(idx1 + 1, len(X)):
+            corr[idx1, idx2] = fechner_corr(X[idx1], X[idx2])
+
+    return corr + corr.T - np.eye(X.shape[0])
+
+def fechner_pvalues(r, n, N=0):
+    r = (r*n + n) / 2
+    stat = (r - 0.5*n) / np.sqrt(0.5*n*0.5)
+
+    pvalue = 2 * norm.sf(np.abs(stat))
+
+    return pvalue
+
+
+class MHTSolver:
+    def __init__(self, alpha, p_val_fun, corr_fun=np.corrcoef):
+        self.alpha = alpha
+        self.p_val_fun = p_val_fun
+        self.corr_fun = corr_fun
+
+    def fit(self, X, y=None):
+        self.n_obs, self.dim = X.shape
+        self.n_tests = self.dim * (self.dim - 1) // 2
+        self.corr_mat = self.corr_fun(X.T)
+        np.fill_diagonal(self.corr_mat, 0)
+        self.p_values = np.vectorize(self.p_val_fun)(self.corr_mat,
+                                                     self.n_obs,
+                                                     self.dim)
+
+    def apply_correction(self, procedure):
+        if procedure == 'SI':
+            return (self.p_values < self.alpha).astype(int)
+
+        if procedure == 'B':
+            return (self.p_values < (self.alpha / self.n_tests)).astype(int)
+
+        if procedure == 'H':
+            triu_idx = np.triu_indices_from(self.p_values, k=1)
+
+            triu_idx_list = np.array(triu_idx).T
+            pvals_list = self.p_values[triu_idx]
+
+            perm = np.argsort(pvals_list)
+
+            idx_sorted = [triu_idx_list[idx] for idx in perm]
+            pvals_sorted = [pvals_list[idx] for idx in perm]
+
+            adj = np.zeros((self.dim, self.dim))
+
+            for k in range(1, self.n_tests + 1):
+                curve_val = self.alpha / (self.n_tests + 1 - k)
+                if pvals_sorted[k - 1] > curve_val:
+                    break
+                adj[tuple(idx_sorted[k - 1])] = 1
+                adj[tuple(idx_sorted[k - 1])[::-1]] = 1
+            return adj
+
+        if procedure == 'BH':
+            triu_idx = np.triu_indices_from(self.p_values, k=1)
+
+            triu_idx_list = np.array(triu_idx).T
+            pvals_list = self.p_values[triu_idx]
+
+            perm = np.argsort(pvals_list)
+
+            idx_sorted = [triu_idx_list[idx] for idx in perm]
+            pvals_sorted = [pvals_list[idx] for idx in perm]
+
+            adj = np.ones((self.dim, self.dim)) - np.eye(self.dim)
+
+            for k in range(self.n_tests, 0, -1):
+                curve_val = self.alpha * k / self.n_tests
+                if pvals_sorted[k - 1] <= curve_val:
+                    break
+                adj[tuple(idx_sorted[k - 1])] = 0
+                adj[tuple(idx_sorted[k - 1])[::-1]] = 0
+            return adj
+
+        if procedure == 'BY':
+            triu_idx = np.triu_indices_from(self.p_values, k=1)
+
+            triu_idx_list = np.array(triu_idx).T
+            pvals_list = self.p_values[triu_idx]
+
+            perm = np.argsort(pvals_list)
+
+            idx_sorted = [triu_idx_list[idx] for idx in perm]
+            pvals_sorted = [pvals_list[idx] for idx in perm]
+
+            adj = np.ones((self.dim, self.dim)) - np.eye(self.dim)
+            harm = np.log(self.n_tests) + np.euler_gamma + 1 / (2*self.n_tests)
+
+            for k in range(self.n_tests, 0, -1):
+                curve_val = self.alpha * k / self.n_tests / harm
+                if pvals_sorted[k - 1] <= curve_val:
+                    break
+                adj[tuple(idx_sorted[k - 1])] = 0
+                adj[tuple(idx_sorted[k - 1])[::-1]] = 0
+            return adj
